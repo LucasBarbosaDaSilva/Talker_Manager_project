4a0cf19259038f6e0306b1dba06ca2a4d869ac19
LucasBarbosaDaSilva <l.barbosatkd@gmail.com>
2023-05-17 14:36:29 -0300

    Iniciando projeto Talker Manager

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
-
-// não remova esse endpoint, e para o avaliador funcionar
+//Hello World 
+// não remova, esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
