fbb3e13db79a0d6c06dda58dcd0aa1e3b44108ac
LucasBarbosaDaSilva <l.barbosatkd@gmail.com>
2023-05-20 17:05:44 -0300

    Feito requisito 2, faltando ajeitar requisito 1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
+const fs = require('fs');
+const { readData, readById } = require('./utils/fcUtils');
+const e = require('express');
 
 const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
-//Hello World 
-// não remova, esse endpoint, e para o avaliador funcionar
+
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
@@ -14,3 +16,19 @@ app.get('/', (_request, response) => {
 app.listen(PORT, () => {
   console.log('Online');
 });
+
+app.get('/talker', async (_req, res) => {
+  const data = await readData();
+  res.status(HTTP_OK_STATUS).json(data) || [];
+});
+
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const data = await readById(id);
+   if (!data) {
+   return res.status(404).json({ message: 'Pessoa palestrante não encontrada' })
+   } else {
+    res.status(HTTP_OK_STATUS).json(data);
+    }
+}
+);
---
+++ src/utils/fcUtils.js
@@ -0,0 +1,29 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+async function readData() {
+  try {
+    const data = await fs.readFile(path.resolve(__dirname, '../data/talker.json'));
+    const result = JSON.parse(data);
+
+    return result;
+  } catch (error) {
+    console.error(`Erro ao ler o arquivo: ${error}`);
+  }
+}
+
+async function readById(id) {
+  try {
+    const data = await readData();
+    const result = data.find((talker) => talker.id === Number(id));
+
+    return result;
+  } catch (error) {
+    console.error(`Erro ao ler o arquivo: ${error}`);
+  }
+} 
+
+module.exports = {
+  readData,
+  readById,
+};
